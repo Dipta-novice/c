db2e34111bb879e34dfaef974e3d09cace9cc276
Dipta-novice <84554906+Dipta-novice@users.noreply.github.com>
2021-05-21 20:05:58 +0530

    Add files via upload

---
+++ asci-char.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main()
+{  int ascii;
+  char character;
+ printf("enter the ascii value");
+ scanf("%d",&ascii);
+ printf("%d=%c",ascii,ascii);
+    
+    return 0;
+    }
